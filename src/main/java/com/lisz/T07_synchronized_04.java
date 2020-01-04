6a7e07de3dc137d1abd25375735c533231982662
lisz1012 <lisz1012@163.com>
2020-01-04 00:12:23 -0800

    Add notes

--- src/main/java/com/lisz/T07_synchronized_04.java
+++ src/main/java/com/lisz/T07_synchronized_04.java
@@ -3,6 +3,7 @@ package com.lisz;
 public class T07_synchronized_04 {
 	private static int count = 10;
 	
+	// 类文件加载到内存之后会专门生成一个Class类的对象
 	public synchronized static void m() { //static，此时还没有this对象生成，相当于synchronized(T07_synchronized_04.class)
 		count --;
 		System.out.println(Thread.currentThread().getName() + " count = " + count);
