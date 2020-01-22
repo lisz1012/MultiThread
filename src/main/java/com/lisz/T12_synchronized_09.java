d15c410f4ef13a2470164c76d5e0e90f84721828
lisz1012 <lisz1012@163.com>
2020-01-22 00:50:49 -0800

    Add notes

--- src/main/java/com/lisz/T12_synchronized_09.java
+++ src/main/java/com/lisz/T12_synchronized_09.java
@@ -4,7 +4,8 @@ import java.util.concurrent.TimeUnit;
 
 // 两个synchronized锁的是同一个对象，所谓的重入锁就是自己拿到这把锁之后，
 // 不停的加锁加锁再加锁，加好几道，然后都是自己这把锁，锁的是同一个对象。
-// 每去一道锁就计数 -1
+// 每去一道锁就计数 -1，也就是说最里面的一个同步方法执行完，锁其实还没释放
+// 要等最外层的方法执行完了才释放
 public class T12_synchronized_09 {
 
 	private static class Parent {
---
+++ src/main/java/com/lisz/T18_AtomicInteger.java
@@ -0,0 +1,32 @@
+package com.lisz;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+// 凡是以Atomic开头的，都是用CAS来保证线程安全的类
+public class T18_AtomicInteger {
+	AtomicInteger count = new AtomicInteger();
+	void m() {
+		for (int i = 0; i < 10000; i++) {
+			count.incrementAndGet();//count++
+		}
+	}
+	public static void main(String[] args) {
+		T18_AtomicInteger t = new T18_AtomicInteger();
+		List<Thread> threads = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			threads.add(new Thread(t::m, "thread - " + i));
+		}
+		threads.forEach(o->o.start());
+		threads.forEach(o->{
+			try {
+				o.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+		System.out.println(t.count);
+	}
+
+}
