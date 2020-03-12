223a07aac36985767a0c6e16a4d4c449b864bfe2
lisz1012 <lisz1012@163.com>
2020-03-11 21:45:47 -0700

    LockSupport

---
+++ src/main/java/com/lisz/T34_WithoutVolatile.java
@@ -0,0 +1,15 @@
+package com.lisz;
+
+/*
+ * 曾经的面试题：
+ * 实现一个容器，提供两个方法：add和size
+ * 写两个线程，线程1添加10个元素到容器中，线程2实现监控元素的个数，当个数到达5，线程2给出提示并结束
+ * 下面的程序能完成功能吗？
+ */
+public class T34_WithoutVolatile {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
