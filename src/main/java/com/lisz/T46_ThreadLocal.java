5e447ed54bd2db5ca8064bf924ce0fefa83090ba
lisz1012 <lisz1012@163.com>
2020-04-25 20:17:26 -0700

    VarHandle and ThreadLocal

---
+++ src/main/java/com/lisz/T46_ThreadLocal.java
@@ -0,0 +1,32 @@
+package com.lisz;
+
+import java.util.concurrent.TimeUnit;
+
+// 这里一个线程里的修改影响到了另一个线程里的内容，如何做呢？见 T46_ThreadLocal2
+public class T46_ThreadLocal {
+    static volatile Person p = new Person();
+
+    public static void main(String[] args) {
+        new Thread(()->{
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(p.name);
+        }).start();
+
+        new Thread(()->{
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            p.name = "lisi";
+        }).start();
+    }
+
+    static class Person {
+        String name = "zhangsan";
+    }
+}
