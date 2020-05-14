cab58849b9a4af074c4b5c2778dc6e88cecb7f00
lisz1012 <lisz1012@163.com>
2020-05-14 00:38:36 -0700

    Add notes

--- src/main/java/com/lisz/container/T11_SynchronizedQueue_Print_Alternatively.java
+++ src/main/java/com/lisz/container/T11_SynchronizedQueue_Print_Alternatively.java
@@ -5,6 +5,7 @@ import java.util.concurrent.SynchronousQueue;
 
 // 面试题：用多线程且不能用synchronized，交替打印数字和字母：
 // 1a2b3c4d5e6f7g8h9i10j11k12l13m14n15o16p17q18r19s20t21u22v23w24x25y26z
+// 容量为0的BlockingQueue，所以任何时刻put往里放，take往外取都要阻塞
 public class T11_SynchronizedQueue_Print_Alternatively {
     public static void main(String[] args) {
         BlockingQueue<Integer> q = new SynchronousQueue<>();
