364030861ff9bcb0658f2d916010ccd8d29e01dc
lisz1012 <lisz1012@163.com>
2020-05-14 23:32:41 -0700

    Future and FutureTask

---
+++ src/main/java/com/lisz/threadpool/T06_Future.java
@@ -0,0 +1,26 @@
+package com.lisz.threadpool;
+
+import java.util.concurrent.*;
+
+public class T06_Future {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        FutureTask<Integer> task = new FutureTask<>(()->{
+            TimeUnit.MILLISECONDS.sleep(500);
+            return 1000;
+        });
+        new Thread(task).start();
+
+        System.out.println(task.get());
+
+        ExecutorService service = Executors.newFixedThreadPool(5);
+        Future<Integer> f = service.submit(()->{
+            TimeUnit.SECONDS.sleep(2);
+            return 100;
+        });
+        System.out.println(f.get());
+        System.out.println(f.isDone());  // true
+        System.out.println(service.isShutdown());  // false
+        service.shutdown();
+        System.out.println(service.isShutdown());  // true
+    }
+}
