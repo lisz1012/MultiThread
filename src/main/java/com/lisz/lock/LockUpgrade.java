25dbdf31ffa3cb4e4398d497e80cb680ff0328ea
lisz1012 <lisz1012@163.com>
2024-05-16 22:46:52 -0700

    Add jol example

--- src/main/java/com/lisz/lock/LockUpgrade.java
+++ src/main/java/com/lisz/lock/LockUpgrade.java
@@ -4,7 +4,7 @@ import org.openjdk.jol.info.ClassLayout;
 
 public class LockUpgrade {
     public static void main(String[] args) throws  Exception {
-        //Thread.sleep(5000);
+        Thread.sleep(5000);
         Object o = new Object();
         String s = ClassLayout.parseInstance(o).toPrintable();
         System.out.println(s);
